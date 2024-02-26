46eaeb25a80ccebb3ed81f7369a178428d8ee880
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2024-02-26 15:01:21 +0200

    Task/setup-config (#2)
    * Task/setup-config
    
    * updated git ignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppService } from 'src/app.service';
 import { HelloWorldModule } from 'src/hello-world/hello-world.module';
 import { HelloWorldController } from 'src/hello-world/controller/hello-world.controller';
 
 @Module({
-  imports: [HelloWorldModule],
+  imports: [
+    HelloWorldModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+  ],
   controllers: [HelloWorldController],
   providers: [AppService],
 })
