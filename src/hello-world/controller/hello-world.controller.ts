9a0302660b47eaa6c7a30fae0adda743559da887
Liviu Mitrofan <liviujmk@gmail.com>
2024-02-26 14:57:28 +0200

    Task/setup-config

--- src/hello-world/controller/hello-world.controller.ts
+++ src/hello-world/controller/hello-world.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class HelloWorldController {
+  constructor(private configService: ConfigService) {}
+  configTest = this.configService.get<string>('CONFIG_TEST');
+
   @Get()
   helloWorld() {
-    return { msg: 'Hello World' };
+    return { msg: this.configTest };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3200;
+
+  await app
+    .listen(port)
+    .then(() => console.log(`App started on http://localhost:${port}`));
 }
 bootstrap();
