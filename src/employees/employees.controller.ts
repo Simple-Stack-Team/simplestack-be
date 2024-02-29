39ae0adc415b239d6ad798c8f250759b0d726994
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2024-02-29 13:55:02 +0200

    Added basic role system (#5)

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/types/role.types';
 import { EmployeesService } from 'src/employees/employees.service';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Roles(Role.EMPLOYEE)
   @Get(':id')
   async getEmployee(@Param('id') id: string) {
     return await this.employeesService.getEmployeeById(id);
