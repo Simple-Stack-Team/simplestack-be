2a8ce4460d9b71bba27d7aef6c0ab6349f4f764a
Liviu Mitrofan <liviujmk@gmail.com>
2024-02-29 13:52:31 +0200

    Added basic role system

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
-import { EmployeesService } from './employees.service';
-import { EmployeesController } from './employees.controller';
+import { EmployeesService } from 'src/employees/employees.service';
+import { EmployeesController } from 'src/employees/employees.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RolesGuard } from 'src/auth/guards/role.guard';
 
 @Module({
   controllers: [EmployeesController],
-  providers: [EmployeesService, PrismaService],
+  providers: [
+    EmployeesService,
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
   exports: [EmployeesService],
 })
 export class EmployeesModule {}
