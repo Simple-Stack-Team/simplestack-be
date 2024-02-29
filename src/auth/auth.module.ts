f76c185871ffa8125a81eb06d2639b6242f22cba
Liviu Mitrofan <64259428+Liviujmk@users.noreply.github.com>
2024-02-29 17:28:54 +0200

    Added organization team roles (#6)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
 
 import { AuthService } from 'src/auth/auth.service';
 import { AuthController } from 'src/auth/auth.controller';
 import { EmployeesService } from 'src/employees/employees.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OrganizationsService } from 'src/organizations/organizations.service';
-import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Module({
   imports: [
@@ -18,6 +16,7 @@ import { AuthGuard } from 'src/auth/guards/auth.guard';
         secret: configService.get('JWT_SECRET'),
         signOptions: { expiresIn: '3600s' },
       }),
+      global: true,
       inject: [ConfigService],
     }),
   ],
@@ -26,10 +25,6 @@ import { AuthGuard } from 'src/auth/guards/auth.guard';
     EmployeesService,
     PrismaService,
     OrganizationsService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
   ],
   controllers: [AuthController],
 })
--- src/organizations/organizations.service.ts
+++ src/organizations/organizations.service.ts
@@ -1,5 +1,5 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import { Organization } from '@prisma/client';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { Organization, TeamRole } from '@prisma/client';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -19,7 +19,70 @@ export class OrganizationsService {
         },
       });
     } catch (error) {
-      throw new HttpException('Server error', 505);
+      throw new HttpException('Server error', 500);
     }
+  }
+
+  async createOrganizationTeamRoles(
+    name: string,
+    orgId: string,
+  ): Promise<TeamRole> {
+    const org = await this.prismaService.organization.findUnique({
+      where: { id: orgId },
+    });
+
+    if (!org) throw new NotFoundException('Organization not found');
+
+    return await this.prismaService.teamRole.create({
+      data: {
+        name,
+        organizationId: orgId,
+      },
+    });
+  }
+
+  async getOrganizationTeamRoles(
+    orgId: string,
+  ): Promise<{ teamRoles: TeamRole[] }> {
+    const orgTeamRoles = await this.prismaService.organization.findFirst({
+      where: {
+        id: orgId,
+      },
+      select: { teamRoles: true },
+    });
+
+    if (!orgTeamRoles) throw new NotFoundException('Organization not found');
+    return orgTeamRoles;
+  }
+
+  async updateTeamRole(
+    orgId: string,
+    teamRoleId: string,
+    updatedName: string,
+  ): Promise<TeamRole> {
+    const teamRole = await this.prismaService.teamRole.update({
+      where: {
+        id: teamRoleId,
+        organizationId: orgId,
+      },
+      data: {
+        name: updatedName,
+      },
+    });
+
+    if (!teamRole) throw new NotFoundException('Team role not found');
+    return teamRole;
+  }
+
+  async deleteTeamRole(orgId: string, teamRoleId: string): Promise<TeamRole> {
+    const teamRole = await this.prismaService.teamRole.delete({
+      where: {
+        id: teamRoleId,
+        organizationId: orgId,
+      },
+    });
+
+    if (!teamRole) throw new NotFoundException('Team role not found');
+    return teamRole;
   }
 }
